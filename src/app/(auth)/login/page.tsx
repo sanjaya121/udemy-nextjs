dccc04df6eb7c846df1a119f51892ee82a3af5fd
“Sanjaya <sanjaya121@gmail.com>
2025-03-26 13:04:34 -0500

    routing modified

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+	return (
+		<>
+			<>Login Page</>
+		</>
+	);
+};
+
+export default Login;
---
+++ src/app/(shop)/products/page.tsx
@@ -0,0 +1,9 @@
+const Prodcuts = (params) => {
+	return (
+		<>
+			<h4> Products Page</h4>
+		</>
+	);
+};
+
+export default Prodcuts;
