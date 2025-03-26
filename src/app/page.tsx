213ad8265c36763d2d450a12224e8a98c6f249cd
“Sanjaya <sanjaya121@gmail.com>
2025-03-26 02:42:25 -0500

    under construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,14 @@ export default function Home() {
 		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
 			<h1 className='text-5xl'> Site under Construction </h1>
 
-			<span>check back later</span>
+			<div className='img-container'>
+				<Image
+					src='/img/construction.svg'
+					alt='hero animation'
+					width={500}
+					height={500}
+				/>
+			</div>
 		</div>
 	);
 }
