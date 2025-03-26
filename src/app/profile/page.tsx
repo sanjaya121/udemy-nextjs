a6274b95b215fb56edcbb07a0c99bbadcd6a1d18
“Sanjaya <sanjaya121@gmail.com>
2025-03-26 11:07:14 -0500

    change logo

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import styles from './profile.module.scss';
+
+export default function Profile() {
+	const [product, setProduct] = useState([]);
+	useEffect(() => {
+		const getDat = async () => {
+			const response = await fetch('https://dummyjson.com/products');
+			const json = await response.json();
+			setProduct(json.products);
+		};
+
+		getDat();
+	}, []);
+	return (
+		console.log('product :>> ', product),
+		(
+			<div className={styles.headingtext}>
+				<div className='products-container'>
+					{product.map((product) => (
+						<div key={product.id}>
+							<li>{product.brand}</li>
+						</div>
+					))}
+				</div>
+			</div>
+		)
+	);
+}
